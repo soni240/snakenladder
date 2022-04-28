14058d0741eba5d4e8692b49ce3d410c3e268079
soni240 <s.fegade21@gmail.com>
2022-04-28 20:38:59 +0530

    diceroll

---
+++ UC2diceroll/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+{
+    static void Main(string[] args)
+    {
+        int playerPos = 0, rollDice;
+        Random random = new Random();
+        rollDice = random.Next(1, 7);
+        Console.WriteLine("Rolled Dice number is:" + rollDice);
+    }
+}
+}
